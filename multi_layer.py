fcce5e83b6212ad486cecea8f1ccc063a1015e99
perborgen <perhb@hotmail.com>
2015-09-27 22:52:25 +0200

    working net

--- multi_layer.py
+++ multi_layer.py
@@ -21,31 +21,42 @@ syn0 = 2 * np.random.random((3,4))-1
 syn1 = 2 * np.random.random((4,1))-1 
 
 
-for j in xrange(1):
+for j in xrange(10001):
 	l0 = X
 	input_l1 = np.dot(l0,syn0)
 	l1 = nonlin(input_l1)
-	print 'l0:'
-	print l0	
-	print 'syn0: '
-	print syn0
-	print 'inplut_l1: '
-	print input_l1
-	print 'l1'
-	print l1
-	print 'syn1'
-	print syn1
 	input_l2 = np.dot(l1,syn1)
 	l2 = nonlin(input_l2)
-	print 'l2'
-	print l2
+
+	#print 'l0:'
+	#print l0	
+	#print 'syn0: '
+	#print syn0
+	#print 'inplut_l1: '
+	#print input_l1
+	#print 'l1'
+	#print l1
+	#print 'syn1'
+	#print syn1
+	#print 'l2'
+	#print l2
+
 
 	l2_error = y - l2
-	l2_error = y - l2
-	if (j% 10000) == 0:
-		print "Error:" + str(np.mean(np.abs(l2_error)))
 	l2_delta = l2_error * nonlin(l2,deriv=True)
+	
 	l1_error = l2_delta.dot(syn1.T)
 	l1_delta = l1_error * nonlin(l1,deriv=True)
 	syn0 += l0.T.dot(l1_delta)
 	syn1 += l1.T.dot(l2_delta)
+
+
+	if (j% 10000) == 0:
+		print "Error:" + str(np.mean(np.abs(l2_error)))
+		print 'l1'
+		print l1
+		print 'l2_delta:'
+		print l2_delta
+
+
+
--- pernet.py
+++ pernet.py
@@ -11,6 +11,8 @@ y = np.array([[0],
 			  [1],
 			  [0]])
 
+np.random.seed(1)
+
 def sigmoid(s):
 	return 1/(1+np.exp(-s))
 
@@ -19,41 +21,67 @@ def sigmoidDeriv(s):
 
 weights_0 = 2*np.random.rand(3,4) - 1
 weights_1 = 2*np.random.rand(4,1) - 1
-for i in xrange(1):
+
+for j in xrange(10001):
 	#forward 
 	l0 = X
-	print 'weights_0'
-	print weights_0
-	print 'l0'
-	print l0
+	#print 'weights_0'
+	#print weights_0
+	#print 'l0'
+	#print l0
 	input_l1 = np.dot(l0,weights_0)
-	print 'input_l1'
-	print input_l1
+	#print 'input_l1'
+	#print input_l1
 	l1 = sigmoid(input_l1)
-	print 'l1'
-	print l1
+	#print 'l1'
+	#print l1
 	input_l2 = np.dot(l1,weights_1)
-	print input_l2
+	#print input_l2
 	l2 = sigmoid(input_l2)
-	print l2
+	#print l2
+
 
 	#backward
 	l2_error = y - l2
-	l2_delta = sigmoidDeriv(l2) * l2_error
-	print 'l2_delta: '
-	print l2_delta
-	print 'weights_1: '
-	print weights_1
-	l1_output_delta = np.dot(l2_delta,weights_1.T)
-	print 'l1_output_delta:'
-	print l1_output_delta
-	l1_input_delta = l1_output_delta * sigmoidDeriv(l1)
-	print 'sigmoidDeriv(l1)'
-	print sigmoidDeriv(l1)
-	print 'l1_input_delta: '
-	print l1_input_delta
+	l2_delta = l2_error*sigmoidDeriv(l2) 
 
-	weights_0_delta = np.dot(l1_input_delta,l0)
+	l1_output_delta = l2_delta.dot(weights_1.T)
+	l1_input_delta = l1_output_delta * sigmoidDeriv(l1)
+
+	weights_0_delta = l0.T.dot(l1_input_delta)
+	weights_1_delta = l1.T.dot(l2_delta)
+
+	weights_0 += weights_0_delta
+	weights_1 += weights_1_delta
+
+	if (j% 10000) == 0:
+		print "Error:" + str(np.mean(np.abs(l2_error)))
+
+	#print 'l2_delta: '
+	#print l2_delta
+	#print 'weights_1: '
+	#print weights_1
+	#print 'l1_output_delta:'
+	#print l1_output_delta
+	#print 'sigmoidDeriv(l1)'
+	#print sigmoidDeriv(l1)
+	#print 'l1_input_delta: '
+	#print l1_input_delta
+	#print 'l0'
+	#print l0
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
